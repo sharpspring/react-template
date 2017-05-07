42f55603ab032549d767ac10d8f5092e914386f3
Michael Sholty <michael.sholty@sharpspring.com>
2017-05-07 10:41:36 -0400

    feat: update setup function to read in project name and replace it in necessary places

--- setup.js
+++ setup.js
@@ -1,56 +1,41 @@
 /*
- * This file should be removed on your project's first commit. If you see this in
- * your project's history at all, someone did something wrong. The only
- * place it should show up at all is in the project starter repo
+ * This file should be removed on your project's first commit.
+ * If you see this in your repo at all, someone did something wrong.
+ * The only place it should show up at all is in the project starter repo.
  */
-var fs = require('fs');
+const fs = require('fs');
 
-function prompt(question, callback) {
-  const stdin = process.stdin,
+const prompt = (question, callback) => {
+  const stdin = process.stdin;
   const stdout = process.stdout;
 
   stdin.resume();
   stdout.write(question);
 
-  stdin.once('data', function(data) {
-    callback(data.toString().trim());
+  stdin.once('data', data => callback(data.toString().trim()));
+}
+
+const setupFile = (fileName, projectName) => {
+  const data = fs.readFileSync(fileName, 'utf-8');
+  const newValue = data.replace(/(\${projectName})/gm, projectName);
+  console.log(newValue);
+  fs.writeFileSync(fileName, newValue, 'utf-8');
+
+  console.log(`${fileName} is updated`);
+}
+
+const setupRepo = (projectName) => {
+  setupFile('./Jenkinsfile', projectName);
+  setupFile('./package.json', projectName);
+  setupFile('./deploy.js', projectName);
+  fs.unlink('./setup.js', (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('setup.js has been deleted');
+    console.log('Setup is complete. Please commit these changes to get started!');
   });
 }
 
-function readWriteAsync() {
-  fs.readFile('filelist.txt', 'utf-8', function(err, data) {
-    if (err) throw err;
-
-    var newValue = data.replace(/^\./gim, 'myString');
-
-    fs.writeFile('filelistAsync.txt', newValue, 'utf-8', function(err) {
-      if (err) throw err;
-      console.log('filelistAsync complete');
-    });
-  });
-}
-
-function readWriteSync() {
-  var data = fs.readFileSync('filelist.txt', 'utf-8');
-
-  var newValue = data.replace(/^\./gim, 'myString');
-
-  fs.writeFileSync('filelistSync.txt', newValue, 'utf-8');
-
-  console.log('readFileSync complete');
-}
-
-console.log('Please enter the name of your new project:');
-const projectName = getProjectName();
-const repoLocation = `https://github.com/sharpspring/${projectName}`;
-setupJenkinsfile();
-setupPackage();
-setupDeploy();
-fs.unlink('./setup.js', (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('setup.js has been deleted');
-  console.log('Setup is complete. Please commit these changes to get started!');
-});
+prompt('Please enter the name of your new project:', setupRepo);
 
