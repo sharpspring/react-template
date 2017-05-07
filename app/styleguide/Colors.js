0c2480e1bcdcc77f05eb2cfa7296926f5d72dde2
Michael Sholty <michael.sholty@sharpspring.com>
2017-05-07 11:20:13 -0400

    feat: add styleguide placeholder

---
+++ app/styleguide/Colors.js
@@ -0,0 +1,21 @@
+export const brandColor = '#84c340';
+export const brandSecondary = '#40acc3';
+
+const colors = {
+  white: '#ffffff',
+  lightGrey: '#D1D6D8',
+  brandColor,
+  brandSecondary,
+  brandColorDark: '#78b13b',
+  headerIconColor: '#485252',
+  textColor: '#555555',
+  headerColor: '#485252',
+  borderColor: '#e0e0e0',
+  borderRightColor: '#cccccc',
+  inputBorderColor: '#C3C8C8',
+  linkColor: '#40acc3',
+  linkColorHover: '#2c7a8b',
+  basicButton: '#a8b0b0',
+};
+
+export default colors;
