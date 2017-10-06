6b6cd5a940fde2cf40c431f7f62d60836eac139b
Harry Gogonis <hgogonis@gmail.com>
2017-10-06 15:01:10 -0400

    fix: fix cache length

--- scoutfile/deploy.js
+++ scoutfile/deploy.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 function deploy(bucket) {
   const source = 'dist/scout.js';
   const destination = `gs://${bucket}/scout.js`;
-  const cmd = `gsutil -h 'Cache-Control:public, max-age=31536000' cp -a public-read ${source} ${destination}`;
+  const cmd = `gsutil -h 'Cache-Control:public, max-age=500' cp -a public-read ${source} ${destination}`;
   exec(cmd, function(error) {
     if(error) {
       console.log(error);
